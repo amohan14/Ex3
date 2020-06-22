39c554ca4e2de0211baca588cc938c6c3fc0d9e7
amohan14 <ananya.mhty@gmail.com>
2020-06-21 23:54:59 -0400

    Added Four Functions

---
+++ calc.py
@@ -0,0 +1,15 @@
+def addition4(w = 8, x = 4, y = 2, z = 1):
+    op = w + x + y + z
+    print(op)
+
+def subtract4(w = 8, x = 4, y = 2, z = 1):
+    op = w - x - y - z
+    print(op)
+
+def multiply4(w = 8, x = 4, y = 2, z = 1):
+    op = w * x * y * z
+    print(op)
+
+def divide4(w = 8, x = 4, y = 2, z = 1):
+    op = (w / x / y / z)
+    print(op)
